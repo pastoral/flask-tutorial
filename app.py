8273a583cb35919ddb49b0b9a2c0f06bb64987b3
Munir <munir@Munirs-MacBook-Pro.local>
2026-02-11 15:59:12 +0600

    processing the POST Login and redirect to the Profile page

--- app.py
+++ app.py
@@ -6,9 +6,19 @@ app = Flask(__name__, template_folder = 'templates')
 def index():
     return render_template("index.html")
 
+@app.route("/user/<name>")
+def userprofile(name):
+    return render_template("userprofile.html", name=name)
+
+
 @app.route("/login", methods=['GET'])
 def login():
     return render_template("login.html")
+
+@app.route("/process_login", methods=["POST"])
+def processing_login():
+    name = request.form["username"]
+    return redirect(url_for("userprofile" , name = name))
 
 if __name__ == "__main__":
     app.run(debug=True)

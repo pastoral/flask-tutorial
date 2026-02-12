c0c61388f87610a2edd2ceacf48d331722b37011
Munir <munir@Munirs-MacBook-Pro.local>
2026-02-12 12:37:54 +0600

    added another Login page, GET and POST methods on same route

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def processing_login():
     name = request.form["username"]
     return redirect(url_for("userprofile" , name = name))
 
+@app.route("/home", methods = ["GET","POST"])
+def home_page():
+    if request.method=="GET":
+        return render_template("home.html")
+    elif request.method=="POST":
+        return redirect(url_for('userprofile', name = request.form["username"]))
+
+
 if __name__ == "__main__":
     app.run(debug=True)

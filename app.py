823bfa867e0336c7bed7724915f4cd6e7ae56cb0
Munir <munir@Munirs-MacBook-Pro.local>
2026-02-13 19:02:38 +0600

    File uploading options added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -11,7 +12,7 @@ def userprofile(name):
     return render_template("userprofile.html", name=name)
 
 
-@app.route("/login", methods=['GET'])
+@app.route("/login")
 def login():
     return render_template("login.html")
 
@@ -26,6 +27,17 @@ def home_page():
         return render_template("home.html")
     elif request.method=="POST":
         return redirect(url_for('userprofile', name = request.form["username"]))
+
+
+@app.route("/file_upload", methods = ["POST"])
+def file_upload():
+    file = request.file["filename"]
+    if file.content_type == "text/plain":
+        return file.read().decode()
+
+    elif file.content_type == "application/vnd.ms-excel" or file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        df = pd.read_excel(file)
+        return df.to_html()
 
 
 if __name__ == "__main__":

1a57317674737e9f44a4d409b41ae465d6c91d24
Munir <munir@Munirs-MacBook-Pro.local>
2026-03-23 22:22:37 +0600

    Cookie Data added and retrieved

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, Response, jsonify, session
+from flask import Flask, render_template, request, redirect, url_for, Response, jsonify, session, make_response
 
 
 app = Flask(__name__, template_folder = 'templates', static_folder='static', static_url_path = '/')
@@ -18,9 +18,18 @@ def userlogin():
     elif request.method=="POST":
         #return redirect(url_for("dashboard", username=request.form["username"])) ##
         username = request.form["username"]
-        # Save user in session
+        # Save user in session ####
         session["user"] = username
-        return redirect(url_for("dashboard"))
+        
+        # Save user data in Cookies ####
+        ## create the redirect response, set cookies on that response, and then return the response at the end ###
+        resp = make_response(redirect(url_for("dashboard")))
+        
+        resp.set_cookie("lang_pref", "Spanish")
+        resp.set_cookie("theme_color", "Dark")
+        
+        # return redirect(url_for("dashboard")) ##
+        return resp
 
 @app.route('/dashboard')
 def dashboard():
@@ -29,8 +38,17 @@ def dashboard():
     ### GET the User data from the Session ###
     if "user" in session:
         name = session["user"]
+        
+        ## Get the Cookie Data ##
+        lang_pref = request.cookies["lang_pref"]
+        theme_color = request.cookies["theme_color"]
         ## Below two lines ensure accessing the Dashboard only if the user is logged in ##
-        return render_template('dashboard.html', username=name) 
+        return render_template(
+            'dashboard.html', 
+            username=name,
+            lang_pref = lang_pref,
+            theme_color = theme_color
+            ) 
     return redirect(url_for("userlogin"))
 
 

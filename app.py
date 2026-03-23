a979f74540e0846029873494bb022762246e5d79
Munir <munir@Munirs-MacBook-Pro.local>
2026-03-23 20:01:37 +0600

    basic Login system created

--- app.py
+++ app.py
@@ -8,5 +8,22 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login",methods=["GET", "POST"])
+def userlogin():
+    if request.method=="GET":
+        return render_template("login.html")
+    elif request.method=="POST":
+        return redirect(url_for("dashboard", username=request.form["username"]))
+
+@app.route('/dashboard')
+def dashboard():
+    name = request.args.get("username")
+    return render_template('dashboard.html', username=name)
+
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for("userlogin"))
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/hello.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    setTimeout(() => {
-        alert("Hello World")
-    }, 3000);
-}
